a2a09a131f86080757884cd557fa769ee76ac4fb
Jay kadam <166234881+Jaykadam12@users.noreply.github.com>
2025-06-20 18:05:11 +0530

    Update addItems.jsx

--- frontend/pages/addItems.jsx
+++ frontend/pages/addItems.jsx
@@ -14,7 +14,7 @@ function AddItems() {
     console.log("📦 Sending:", data);
 
     try {
-      const res = await fetch("http://localhost:5000/api/items", {
+      const res = await fetch("https://products-assignment-backend.onrender.com/api/items", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
