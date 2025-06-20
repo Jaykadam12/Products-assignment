9c008b97022723a1f55796ae390bb22ba92b115b
Jay kadam <166234881+Jaykadam12@users.noreply.github.com>
2025-06-20 20:48:31 +0530

    Update navBar.jsx

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 function NavBar() {
   return (
-    <div className="flex justify-between px-32 bg-gray-400 text-white py-5 items-center">
-      <div className="text-3xl font-bold cursor-pointer">E-Store</div>
-      <div className="flex gap-5 text-lg font-bold">
+    <div className="flex justify-between px-5 md:px-32 bg-gray-400 text-white py-5 items-center">
+      <div className="md:text-3xl text-xl font-bold cursor-pointer">E-Store</div>
+      <div className="flex gap-5 text-md md:text-lg font-bold">
         <Link to={'/'}>View Items</Link>
         <Link to={'/addItems'}>Add Items</Link>
       </div>
